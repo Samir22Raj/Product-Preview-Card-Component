87064f2bfc3c6c7310c9a0b0c9d4843673c5fced
Samir Rajkarnikar <samirrajkarnikar9@gmail.com>
2024-03-18 13:34:44 +0545

    Completed product preview card component

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["*"],
+	theme: {
+		colors: {
+			darkCyan: "hsl(158, 36%, 37%)",
+			Cream: "hsl(30, 38%, 92%)",
+			veryDarkBlue: "hsl(212, 21%, 14%)",
+			dGrayishBlue: "hsl(228, 12%, 48%)",
+			White: "hsl(0, 0%, 100%)",
+		},
+		fontFamily: {
+			Montserrat: "Montserrat",
+			Fraunces: "Fraunces",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
